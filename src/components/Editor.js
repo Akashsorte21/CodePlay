bdc70d288292f1fe4265ee4e4d6667d456c26815
akashsorte <akashsorte1999@gmail.com>
2023-10-10 14:10:02 +0530

    first commit

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,35 +1,29 @@
-import React, { useEffect } from 'react';
 import { Row, Col } from 'antd';
-import codebeelogo from '../assets/codebee-logo.png';
 import {IoLogoHtml5,IoLogoCss3} from 'react-icons/io';
 import {IoLogoJavascript} from 'react-icons/io5';
 import {updateHtmlCode,updateCssCode,updateJsCode} from '../Slices/CodeSlice';
 import { useDispatch,useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+
 function Editor() {
     const htmlCode=useSelector((state)=>state.code.htmlCode);
     const cssCode=useSelector((state)=>state.code.cssCode);
     const jsCode=useSelector((state)=>state.code.jsCode);
     const dispatch=useDispatch();
-    const navigate=useNavigate();
     const handleOutput=()=>{
         var output=document.getElementById('codebee-output');
         output.contentDocument.body.innerHTML=htmlCode+"<style>"+cssCode+"</style>";
         output.contentWindow.eval(jsCode);
     }
-    useEffect(()=>{
-        handleOutput();
-        // eslint-disable-next-line
-    },[])
+
     return (
         <>
             <Row>
                 <Col span={24} className='d-flex justify-content-center align-items-center pt-4 pb-3'>
-                    <img src={codebeelogo} alt="codebee-logo" style={{ width: "150px" }} />
+                            <h1 style={{color:"white"}}>CodePlay</h1>
                 </Col>
             </Row>
             <Row>
-                <Col xs={{span:"22",offset:1}} sm={{span:"22",offset:1}} md={{span:"22",offset:1}} lg={{span:7,offset:.5}}>
+            <Col xs={{span:"22",offset:1}} sm={{span:"22",offset:1}} md={{span:"22",offset:1}} lg={{span:7,offset:.5}}>
                     <div className='code-container'>   
                         <IoLogoHtml5 style={{color:"red",fontSize:"25px"}}/><span>HTML</span>
                         <textarea rows={12} value={htmlCode} onChange={(e)=>{
@@ -53,11 +47,8 @@ function Editor() {
                         }}></textarea>
                     </div>
                 </Col>
-                <Col span={24} className="d-flex justify-content-center align-items-center w-100 mb-4">
+                <Col span={24} className="d-flex justify-content-center align-items-center w-100 mb-4 pt-4">
                     <button onClick={handleOutput}>Run Code</button>
-                    <button onClick={()=>{
-                        navigate('/preview')
-                    }}>Output Window</button>
                 </Col>
             </Row>
             <Row>
